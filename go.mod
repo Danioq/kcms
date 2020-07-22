2680e3948efe2861b8dc8c7a5eb66e58be411f01
Daniel Łobacz <daniok603250@gmail.com>
2020-07-22 14:26:25 +0200

    hello world http

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/Danioq/kcms
+
+go 1.14
+
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/mitranim/gow v0.0.0-20200610154951-cc77cb621df0 // indirect
+	golang.org/x/sys v0.0.0-20200720211630-cb9d2d5c5666 // indirect
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+
+	"fmt"
+	"net/http"
+)
+
+//Index - Hello world
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello World")
+}
+
+//HelloNumber - get number
+func HelloNumber(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, id)
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/{id}", HelloNumber)
+
+	http.ListenAndServe(":8080", router)
+}
